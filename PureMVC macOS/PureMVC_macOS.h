82394648c733f06673e71f057ab0e652348ad4e3
flavor <haker-haker@163.com>
2018-01-01 03:49:12 +0800

    update

---
+++ PureMVC macOS/PureMVC_macOS.h
@@ -0,0 +1,19 @@
+//
+//  PureMVC_macOS.h
+//  PureMVC macOS
+//
+//  Created by flavor on 2018/1/1.
+//  Copyright © 2018年 zhuyanbin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PureMVC_macOS.
+FOUNDATION_EXPORT double PureMVC_macOSVersionNumber;
+
+//! Project version string for PureMVC_macOS.
+FOUNDATION_EXPORT const unsigned char PureMVC_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PureMVC_macOS/PublicHeader.h>
+
+
